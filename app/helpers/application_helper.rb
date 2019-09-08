7f48632be579e6008ab42a1e209b9502c49cd7fa
haru <hitoshi020202@gmail.com>
2019-09-08 16:31:31 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  #ページ毎にタイトルを返す
+  def full_title(page_name = "") # メソッドと因数の定義
+    base_title = "AtendanceApp" #基本となるアプリケーション名を変数に代入  
+    if page_name.empty? #因数を受け取っているか判定
+      base_title #因数page_nameが空文字の場合はbase_titleのみ返す
+    else # 因数page_nameが空文字でない場合
+      page_name + " | " + base_title # 文字列を連結して返す
+    end 
+  end  
 end
